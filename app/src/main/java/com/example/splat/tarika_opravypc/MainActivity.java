98f50e427252b815831966c102c528bd658d9ceb
Cryth <pattar19@icloud.com>
2018-04-02 21:11:58 +0200

    pridanie novej activity pre PCs, pridanie listenera na list a pridanie menu

--- app/src/main/java/com/example/splat/tarika_opravypc/MainActivity.java
+++ app/src/main/java/com/example/splat/tarika_opravypc/MainActivity.java
@@ -1,9 +1,17 @@
 package com.example.splat.tarika_opravypc;
 
+import android.content.Intent;
+import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,8 +25,10 @@ public class MainActivity extends AppCompatActivity {
         setTitle("Opravy počítačov - Zákazníci");
         PripojAdapter();
         //PridajCustomers();
-        //PridajListener();
+        PridajListener();
     }
+
+
 
     private void PridajCustomers() {
         Customer c = new Customer(0, "Patrik Tariška", "pattar19@icloud.com");
@@ -32,4 +42,37 @@ public class MainActivity extends AppCompatActivity {
         ListView lv = findViewById(R.id.listcustomer);
         lv.setAdapter(myAdapt);
     }
+
+    private void PridajListener() {
+        AdapterView.OnItemClickListener clickListener = new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                ListView lv = findViewById(R.id.listcustomer);
+                Cursor c = ((SimpleCursorAdapter)lv.getAdapter()).getCursor();
+                c.moveToPosition(i);
+                long id = c.getLong(c.getColumnIndex(MojaDat.Customers.COLUMN_ID));
+                startActivity(new Intent(getApplicationContext(), computersActivity.class).putExtra("index", id));
+            }
+        };
+        ListView lv = findViewById(R.id.listcustomer);
+        lv.setOnItemClickListener(clickListener);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu_customer, menu);
+        return true;
+    }
+
+
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()) {
+            case R.id.addCustomer:
+                //doplnit activitu
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
 }
